68584db0e9591319804e725d3d2f2946186ee81b
Pranavtripathi-STAR <pranavtripathi0909@gmail.com>
2026-04-19 07:02:17 +0530

    Initial commit

---
+++ client/src/pages/About/About.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./About.css";
+import Jump from "react-reveal/Jump";
+import myPhoto from "../../assets/images/myPhoto.jpg";
+const About = () => {
+  return (
+    <>
+      <Jump>
+        <div className="about" id="about">
+          <div className="row">
+            <div className="col-md-6 col-xl-6 col-lg-6 col-xs-12 about-img">
+              <img
+                src={myPhoto}
+                alt="profile_pic"
+              />
+            </div>
+            <div className="col-md-6 col-xl-6 col-lg-6 col-xs-12 about-content">
+              <h1>About me</h1>
+              <p>
+                I am an aspiring Software Developer with a strong interest in Web Development and AI/ML.
+                I have a solid foundation in C and JavaScript.
+                Currently, I am strengthening my skills in Java and Data Structures & Algorithms.
+                I have built a full-stack project, StudyNotion, gaining experience in frontend and backend development.
+                I am also learning AI and Machine Learning using Python and related tools.
+                I am a quick learner, disciplined, and passionate about solving problems.
+                My goal is to build impactful solutions and grow as a developer.
+              </p>
+            </div>
+          </div>
+        </div>
+      </Jump>
+    </>
+  );
+};
+
+export default About;
