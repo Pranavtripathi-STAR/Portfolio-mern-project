4edd32b53b1bad42a2d24c7a6b1b1ebb3846c66e
Pranavtripathi-STAR <pranavtripathi0909@gmail.com>
2026-04-19 07:50:41 +0530

    added frontend build

--- server.js
+++ server.js
@@ -1,24 +1,27 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const path = require("path");
 
-//dotenv configuartion
 dotenv.config();
 
-//rest object
 const app = express();
 
-//midlewares
 app.use(cors());
 app.use(express.json());
 
-//routes
+// API routes
 app.use("/api/v1/portfolio", require("./routes/portfolioRoute"));
 
-//port
+// Serve frontend
+app.use(express.static(path.join(__dirname, "client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build/index.html"));
+});
+
 const PORT = process.env.PORT || 8080;
 
-//listen
 app.listen(PORT, () => {
-  console.log(`Server Runnning On PORT ${PORT} `);
+  console.log(`Server Running on PORT ${PORT}`);
 });
