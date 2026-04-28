e18386900215774de0f93c80a5580a40ec0fd550
Pranavtripathi-STAR <pranavtripathi0909@gmail.com>
2026-04-28 08:36:39 +0530

    fix: about seaction

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -15,7 +15,7 @@ const About = () => {
             <div className="col-md-6 col-xl-6 col-lg-6 col-xs-12 about-content">
               <h1>About me</h1>
               <p>
-                 Hello, my name is Akshat Tripathi. I am an aspiring Software Developer with a strong interest in Web Development and Artificial Intelligence/Machine Learning.
+                 Hello, my name is Pranav Tripathi. I am an aspiring Software Developer with a strong interest in Web Development and Artificial Intelligence/Machine Learning.
                  I have a good foundation in programming languages like C and JavaScript, and I am currently strengthening my skills in Java along with Data Structures and Algorithms.
                  I have worked on a full-stack project called StudyNotion, which helped me gain practical experience in frontend and backend development. Currently,
                  I am also learning AI and Machine Learning using Python and related tools. I am a quick learner, disciplined, and passionate about solving problems.
